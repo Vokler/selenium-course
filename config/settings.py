d2e28ae6dd826af74845d886a3f315a54acad1a7
Valerii Pavlikov <valerypavlikov@yandex.ru>
2023-04-03 15:01:10 +0300

    Created first test

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+DRIVER_PATH = '/Users/vokler/projects/WebDriver/chromedriver'
--- tests/test_store.py
+++ tests/test_store.py
@@ -1,10 +1,35 @@
+import time
+import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from config.settings import DRIVER_PATH, BASE_DIR
 
-DRIVER_PATH = '/Users/vokler/projects/WebDriver/chromedriver'
+
+@pytest.fixture
+def root_url():
+    return f'file:///{BASE_DIR / "store-template" / "index.html"}'
 
 
-def test_browser():
-    browser = webdriver.Chrome(executable_path=DRIVER_PATH)
+def test_titles_are_correct(root_url):
+    service = Service(executable_path=DRIVER_PATH)
+    browser = webdriver.Chrome(service=service)
     browser.maximize_window()
-    browser.get("https://www.google.com")
+
+    time.sleep(2)
+    browser.get(root_url)
+
+    time.sleep(2)
+    main_title = browser.find_element(By.CLASS_NAME, 'navbar-brand')
+    assert main_title.text == 'Store'
+
+    time.sleep(2)
+    purchase_link = browser.find_element(By.ID, 'start-purchase-link')
+    purchase_link.click()
+
+    time.sleep(2)
+    product_title = browser.find_element(By.ID, 'product-title')
+    assert product_title.text == 'Store'
+
+    time.sleep(2)
     browser.quit()
